482995ce4e07b53f263ab09bda7508bd6cfd3445
Blacklight <bilalabdlhamid6@gmail.com>
2025-06-25 21:27:56 +0200

    Debugging...

--- hypeapp/app/index.jsx
+++ hypeapp/app/index.jsx
@@ -1,36 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
-import { getAuth, onAuthStateChanged } from '@react-native-firebase/auth';
+import React, { useState } from 'react';
+import { TextInput, Button, SafeAreaView } from 'react-native';
+import { getAuth, createUserWithEmailAndPassword  } from '@react-native-firebase/auth';
 
 export default function Index() {
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
 
-  // Handle user state changes
-  function handleAuthStateChanged(user) {
-    setUser(user);
-    if (initializing) setInitializing(false);
+  const createUser = ({email, password}) => {
+
+    console.log(`${email}, ${password}`);
+
+  createUserWithEmailAndPassword(getAuth(), email, password)
+    .then(() => {
+      console.log('User account created & signed in!');
+    })
+    .catch(error => {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('That email address is already in use!');
+      }
+
+      if (error.code === 'auth/invalid-email') {
+        console.log('That email address is invalid!');
+      }
+
+      console.error(error);
+    });
   }
 
-  useEffect(() => {
-    const subscriber = onAuthStateChanged(getAuth(), handleAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
 
-  if (initializing) return null;
-
-  if (!user) {
-    return (
-      <View>
-        <Text>Login</Text>
-      </View>
-    );
-  }
+  const [email, setEmail] = useState(''); 
+  const [password, setPassword] = useState(''); 
 
   return (
-    <View>
-      <Text>Welcome {user.email}</Text>
-    </View>
-  );
+    <SafeAreaView style={{flex: 1, justifyContent: 'center', backgroundColor: 'green'}}>
+      <TextInput
+      value={email}
+      onChangeText={setEmail}
+      style={{borderWidth: 3}}
+      />
+      <TextInput
+      value={password}
+      onChangeText={setPassword}
+      style={{borderWidth: 3}}
+      />
+      <Button title='createUser' onPress={()=>{createUser({email, password})}} />
+    </SafeAreaView>
+  )
 }
